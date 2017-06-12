f7d731736dc19dbef859d960c65549f7bb6f0a88
Stephen Swatman <stephenswat@gmail.com>
2017-06-12 16:09:05 +0200

    Add a Black Ops preset.

--- include/parameters.hpp
+++ include/parameters.hpp
@@ -27,28 +27,29 @@ enum fatigue_model {
 
 class Parameters {
 public:
-    Parameters(double j, double w, double a, double g) {
-        this->jump_range = j;
+    Parameters(double w, double a, double g, double j) {
         this->warp_speed = w;
         this->align_time = a;
         this->gate_cost = g;
-        this->fatigue_model = FATIGUE_REACTIVATION_COUNTDOWN;
+        this->jump_range = j;
     }
 
-    double jump_range, warp_speed, align_time, gate_cost;
-    enum fatigue_model fatigue_model;
+    double jump_range = NAN, warp_speed, align_time = 0.0, gate_cost;
+    enum fatigue_model fatigue_model = FATIGUE_REACTIVATION_COUNTDOWN;
 };
 
-static const Parameters FRIGATE = Parameters(NAN, 5.0, 3.0, 10.0);
-static const Parameters DESTROYER = Parameters(NAN, 4.5, 4.0, 10.0);
-static const Parameters CRUISER = Parameters(NAN, 3.0, 7.0, 10.0);
-static const Parameters BATTLECRUISER = Parameters(NAN, 2.7, 8.0, 10.0);
-static const Parameters BATTLESHIP = Parameters(NAN, 2.0, 12.0, 10.0);
+static const Parameters FRIGATE = Parameters(5.0, 3.0, 10.0, NAN);
+static const Parameters DESTROYER = Parameters(4.5, 4.0, 10.0, NAN);
+static const Parameters CRUISER = Parameters(3.0, 7.0, 10.0, NAN);
+static const Parameters BATTLECRUISER = Parameters(2.7, 8.0, 10.0, NAN);
+static const Parameters BATTLESHIP = Parameters(2.0, 12.0, 10.0, NAN);
 
-static const Parameters CARRIER = Parameters(7.0, 1.5, 30.0, -1.0);
-static const Parameters DREADNOUGHT = Parameters(7.0, 1.5, 40.0, -1.0);
-static const Parameters SUPERCARRIER = Parameters(6.0, 1.5, 40.0, -1.0);
-static const Parameters TITAN = Parameters(6.0, 1.37, 60.0, -1.0);
+static const Parameters BLACK_OPS = Parameters(2.2, 10.0, 10.0, 8.0);
 
-static const Parameters RORQUAL = Parameters(10.0, 1.5, 50.0, -1.0);
-static const Parameters JUMP_FREIGHTER = Parameters(10.0, 1.5, 40.0, -1.0);
+static const Parameters CARRIER = Parameters(1.5, 30.0, -1.0, 7.0);
+static const Parameters DREADNOUGHT = Parameters(1.5, 40.0, -1.0, 7.0);
+static const Parameters SUPERCARRIER = Parameters(1.5, 40.0, -1.0, 6.0);
+static const Parameters TITAN = Parameters(1.37, 60.0, -1.0, 6.0);
+
+static const Parameters RORQUAL = Parameters(1.5, 50.0, -1.0, 10.0);
+static const Parameters JUMP_FREIGHTER = Parameters(1.5, 40.0, -1.0, 10.0);
