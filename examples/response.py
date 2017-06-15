a00f94dcf7c1e79ec8236ae26641fc7ec4473719
Stephen Swatman <stephenswat@gmail.com>
2017-06-15 16:47:40 +0200

    Flesh out the response.py example.

--- examples/response.py
+++ examples/response.py
@@ -2,20 +2,50 @@ import eve_nerd
 import numpy as np
 import matplotlib.pyplot as plt
 
+LY_TO_M = 9460730472580800.0
+
 a = eve_nerd.Universe("mapDenormalize.csv", "mapJumps.csv")
-a.add_dynamic_bridge(40199046, 6.0)
-b = a.get_all_distances(30003135, eve_nerd.JUMP_FREIGHTER)
+# a.add_dynamic_bridge(40199046, 6.0)
 
-sys = []
-res = {}
+cases = [
+    ("Frigates", eve_nerd.FRIGATE),
+    ("Cruisers", eve_nerd.CRUISER),
+    ("Battlecruisers", eve_nerd.BATTLECRUISER),
+    ("Battleships", eve_nerd.BATTLESHIP),
+    ("Carriers", eve_nerd.CARRIER),
+    ("Supercarriers", eve_nerd.SUPERCARRIER),
+]
+fig = plt.figure(figsize=(10, 10), dpi=300)
 
-for e, d in b.items():
-    c = (e.system.id, e.system.x, e.system.z)
-    if d < res.get(c, float("inf")):
-        res[c] = d
+for i, (n, shiptype) in enumerate(cases):
+    b = a.get_all_distances(30003135, shiptype)
 
-        if c not in sys:
-            sys.append(c)
+    sys = []
+    res = {}
 
-plt.scatter([x[1] for x in sys], [x[2] for x in sys], c=[res[x] for x in sys], alpha=0.5, cmap='RdYlBu', vmin=0, vmax=1800, s=[256.0 if x[0] in [30003135, 30001162] else 16.0 for x in sys])
-plt.show()
+    for e, _d in b.items():
+        c = (e.system.id, e.system.x / LY_TO_M, e.system.z / LY_TO_M)
+        d = _d / 60
+        if d < res.get(c, float("inf")):
+            res[c] = d
+
+            if c not in sys:
+                sys.append(c)
+
+
+    ax = fig.add_subplot(3, 2, i + 1)
+    ax.set_aspect('equal', 'datalim')
+    ax.set_title(n)
+
+    p = ax.scatter(
+        [x[1] for x in sys],
+        [x[2] for x in sys],
+        c=[res[x] for x in sys],
+        alpha=0.5, cmap='RdYlBu', vmin=0, vmax=30,
+        s = 1.0
+    )
+
+    cbar = fig.colorbar(p)
+    cbar.solids.set_edgecolor("face")
+
+plt.savefig('foo.png')
